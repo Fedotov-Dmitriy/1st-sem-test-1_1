e99a83a5c007790d07a8aa4a88188330d2e4b006
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-10-23 14:39:52 +0300

    Add exceptions handling and tests for it, fix odd power bug

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,14 @@
 def fast_pow(number, power):
-    result = number
-    while power != 1:
-        result *= result
+    assert (isinstance(number, int) or isinstance(number, float) \
+            and isinstance(power, int)), \
+            "Error: number and power must be numbers"
+
+    assert (power > 0 and isinstance(power, int)), "Error: power must be natural number"
+
+    result = 1
+    while power > 0:
+        if power % 2  == 1:
+            result *= number
+        number *= number
         power = power // 2
     return result
--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -1,9 +1,26 @@
+import pytest
+
 from src.fast_pow import fast_pow
 
 
 def test_two_power_two():
     assert fast_pow(2, 2) == 4
 
+def test_five_power_five():
+    assert fast_pow(5, 5) == 3125
 
 def test_negative():
-    assert fast_pow(-1, 4) == 1
+    with pytest.raises(AssertionError):
+        fast_pow(5, -2)
+
+def test_not_natural():
+    with pytest.raises(AssertionError):
+        fast_pow(6, 1.5)
+
+def test_zero():
+    with pytest.raises(AssertionError):
+        fast_pow(3, 0)
+
+def test_wrong_types():
+    with pytest.raises(AssertionError):
+        fast_pow("foo", "bar")
