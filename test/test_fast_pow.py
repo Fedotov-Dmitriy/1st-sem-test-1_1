4a36f823a9740aa11faf2c27d8946dccba7bef70
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-10-23 14:45:01 +0300

    Add test for fast_pow
    Add test, who tests not natural power.

--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -7,3 +7,10 @@ def test_two_power_two():
 
 def test_negative():
     assert fast_pow(-1, 4) == 1
+
+
+def test_incorrect_power():
+    try:
+        fast_pow(1, -1)
+    except Exception:
+        assert True
