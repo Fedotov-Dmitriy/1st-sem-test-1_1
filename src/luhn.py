a105b7e8de99aeea4c6a52554ea7d9e3507a6dfc
Visheratin Nikita <visheratinn@gmail.com>
2025-10-23 14:47:44 +0300

    ValueError

--- src/luhn.py
+++ src/luhn.py
@@ -2,12 +2,17 @@ def luhn_check(card_number):
     #проверка строки
     if not isinstance(card_number, str):
         raise TypeError("card_number должен быть строкой")
+    #проверка на корректный ввод
 
 
 
 
 
     digits = [int(d) for d in str(card_number) if d.isdigit()]
+
+    if len(digits) < 2:
+        raise ValueError("Как минимум 2 цифры.")
+
     control = digits.pop()
     parity = (len(digits))%2
     total = 0
