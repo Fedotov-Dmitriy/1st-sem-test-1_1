d7d77e8fd2a13b712b283de697fea41c7fa1e99a
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-10-23 14:16:40 +0300

    Add checking for pow < 0

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,4 +1,6 @@
-def fastPow(number, power):
+def fast_pow(number, power):
+    if power < 0:
+        return 1 / fast_pow(number, -power)
     result = number
     while power != 1:
         result *= result
