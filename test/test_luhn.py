711f8501a044e85ce22869dcccf12c203b856928
Nikolay Baboshin <231540704+shknoko@users.noreply.github.com>
2025-10-23 14:43:02 +0300

    Исправлен баг: неправильный тест. Добавлен тест с корректным номером

--- test/test_luhn.py
+++ test/test_luhn.py
@@ -1,9 +1,13 @@
 from src.luhn import luhn_check
 
 
-def test_good():
-    assert luhn_check("8571 2612 1234 5467")
-
-
 def test_bad():
+    assert not luhn_check("8571 2612 1234 5467")
+
+
+def test_another_bad():
     assert not luhn_check("4561 2612 1234 5463")
+
+
+def test_good():
+    assert luhn_check("8571 2612 1234 5466")
