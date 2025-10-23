852e0e9fe4f298b0e32f05dfaf6ff778b6ee2538
Max Ipatov <imaxde@mail.ru>
2025-10-23 14:45:04 +0300

    Exception for negative power

--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -1,9 +1,20 @@
-from src.fast_pow import fastPow
+from src.fast_pow import fast_pow
+import pytest
+import random
 
 
 def test_two_power_two():
-    assert fastPow(2, 2) == 4
+    assert fast_pow(2, 2) == 4
 
 
 def test_negative():
-    assert fastPow(-1, 4) == 1
+    assert fast_pow(-1, 4) == 1
+
+
+@pytest.fixture
+def random_n():
+    return random.randint(1, 100)
+
+
+def test_negative_power(random_n):
+    assert fast_pow(10, -1) == 10 ** (-1)
