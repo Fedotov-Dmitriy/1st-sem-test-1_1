c77aa874ddd83c0708ce4169803965b11a108197
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-10-23 15:06:33 +0300

    Update test for luhn
    Сделаны новые рабочие тесты в связи с исправлением бага в самом алгоритме.

--- test/test_luhn.py
+++ test/test_luhn.py
@@ -2,8 +2,8 @@ from src.luhn import luhn_check
 
 
 def test_good():
-    assert luhn_check("8571 2612 1234 5467")
+    assert luhn_check("4561 2612 1234 5467")
 
 
 def test_bad():
-    assert not luhn_check("4561 2612 1234 5463")
+    assert not luhn_check("4561 2612 1234 5464")
