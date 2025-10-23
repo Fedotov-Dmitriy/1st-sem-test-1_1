548369c0c9c391d4171dd72c4bbabaeb759bfe1f
ermmmaks <maks.ermoshkin56@gmail.com>
2025-10-23 15:14:57 +0300

    Добавил какую смог утилиту для луны

--- src/luhn.py
+++ src/luhn.py
@@ -12,3 +12,19 @@ def luhnСheck(cardNumber):
         else:
             total += digits[i]
     return (total + control) % 10 == 0
+
+user_number = []
+user_str = str(input("Введите строчку с номером, оканчивающуюся на '-1': "))
+
+def check(str):
+    for i in range(len(str) - 1):
+        if str[i]+str[i+1] != '-1':
+            if str[i] in '0123456789':
+                user_number.append(int(str[i]))
+            else:
+                print("Найден неверный символ: ", str[i])
+    if luhnСheck(user_number):
+        return "Номер корректен"
+    else:
+        return "Некорректный номер"
+print(check(user_str))
