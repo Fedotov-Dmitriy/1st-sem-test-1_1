7e57f322aae286017f7c7331e2693b357f9bf189
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-10-23 14:33:37 +0300

    Fixed bugs in luhn_check()
    card_number must be a string, added control number checking

--- src/luhn.py
+++ src/luhn.py
@@ -1,14 +1,15 @@
-def luhn_check(card_number):
-    digits = [int(d) for d in str(card_number) if d.isdigit()]
+def luhn_check(card_number: str):
+    digits = [int(d) for d in card_number if d.isdigit()]
     control = digits.pop()
     parity = (len(digits)) % 2
     total = 0
     for i in range(len(digits)):
-        if i % 2 == parity:
+        if (i + 1) % 2 == parity:
             doubled = digits[i] * 2
             if doubled > 9:
                 doubled -= 9
             total += doubled
         else:
             total += digits[i]
-    return (total + control) % 10 == 0
+    total += control
+    return total % 10 == 0 and total // 10 == control
