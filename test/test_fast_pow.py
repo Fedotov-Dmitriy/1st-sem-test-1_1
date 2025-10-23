a1b3d9f087c69288364580cfd289306d081e46cc
Nadya Shaporova <saporovanada@gmail.com>
2025-10-23 15:22:25 +0300

    Add unit tests

--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -1,9 +1,11 @@
-from src.fast_pow import fastPow
-
+import pytest
+from fast_pow import fastPow
 
 def test_two_power_two():
     assert fastPow(2, 2) == 4
 
-
 def test_negative():
-    assert fastPow(-1, 4) == 1
+    assert fastPow(256, 0) == 1
+
+def test_neg():
+    assert fastPow(2, -4) == 1
