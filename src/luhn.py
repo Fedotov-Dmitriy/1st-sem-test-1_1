8cbbbfbec090b9b1844107a353171b2ee4308825
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-10-23 14:51:46 +0300

    Added interactive luhn card checking algorithm.

--- src/luhn.py
+++ src/luhn.py
@@ -13,3 +13,32 @@ def luhn_check(card_number: str):
             total += digits[i]
     total += control
     return total % 10 == 0 and total // 10 == control
+
+
+def luhn_interactive():
+    def check_valid_card(card):
+        return all(x.isspace() or x.isdigit() for x in card)
+
+    while True:
+        try:
+            user_input = input("Write card number or -1 to quit: ")
+
+            if user_input == "-1":
+                break
+
+            if not check_valid_card(user_input):
+                raise Exception("You didn't enter a card.")
+
+            if luhn_check(user_input):
+                print("Your card is correct.")
+            else:
+                print("Your card is incorrect.")
+        except EOFError:
+            break
+        except Exception as e:
+            print(e)
+    print("Bye bye!")
+
+
+if __name__ == "__main__":
+    luhn_interactive()
