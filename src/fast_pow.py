5144103f764fb281fe5891365c11d4cd4ab8793b
Kirill Guryanov <kg580652@gmail.com>
2025-10-23 15:02:52 +0300

    программа исправлена, добавлено возведение в отрицательную степень

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,13 @@
 def fast_pow(number, power):
-    result = number
-    while power != 1:
-        result *= result
+    if power == 0:
+        return 1
+    if power < 0:
+        return 1 / fast_pow(number, -power)
+    result = 1
+    base = number
+    while power > 0:
+        if power % 2 == 1:
+            result *= base
+        base *= base
         power = power // 2
     return result
