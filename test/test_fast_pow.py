ace5e208bfd532e0c23ecf893c9f8b4e2682ae9b
Maria Byvalkina <masabyvalkina67715@gmail.com>
2025-10-23 14:41:28 +0300

    Исправили тесты в соответствии с исправленными файлами

--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -1,9 +1,9 @@
-from src.fast_pow import fastPow
+from src.fast_pow import fast_pow
 
 
 def test_two_power_two():
-    assert fastPow(2, 2) == 4
+    assert fast_pow(2, 2) == 4
 
 
 def test_negative():
-    assert fastPow(-1, 4) == 1
+    assert fast_pow(-1, 4) == 1
