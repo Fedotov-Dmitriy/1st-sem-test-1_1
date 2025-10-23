c937f0391aa48c83e44d1e169aa44940bffe9f2e
maxicot <st140678@student.spbu.ru>
2025-10-23 15:17:21 +0300

    Complete test tasks

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,18 @@
-def fastPow(number, power):
-    result = number
-    while power != 1:
-        result *= result
-        power = power // 2
+# Simplistic power implementation for non-negative integer powers
+def fast_pow(number, power: int):
+    if power == 0:
+        if number == 0:
+            raise Exception("0^0 is undefined")
+
+        return 1
+
+    if power < 0:
+        raise Exception("negative power unsupported")
+
+    result = 1
+
+    while power != 0:
+        result *= number
+        power -= 1
+
     return result
--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -1,9 +1,31 @@
-from src.fast_pow import fastPow
+from itertools import product
+
+import pytest
+
+from src.fast_pow import fast_pow
 
 
-def test_two_power_two():
-    assert fastPow(2, 2) == 4
+def test_positive_power():
+    for n, p in product(range(-1000, 1000), range(1, 100)):
+        assert fast_pow(n, p) == n**p
 
 
-def test_negative():
-    assert fastPow(-1, 4) == 1
+def test_negative_power():
+    for n, p in product(range(-1000, 1000), range(1, 100)):
+        with pytest.raises(Exception) as e:
+            fast_pow(n, -p)
+            assert e == "negative power unsupported"
+
+
+def test_zero_power():
+    for n in range(-1000, 100):
+        if n == 0:
+            continue  # tested separately
+
+        assert fast_pow(n, 0) == 1
+
+
+def test_0_pow_0():
+    with pytest.raises(Exception) as e:
+        fast_pow(0, 0)
+        assert e == "0^0 is undefined"
