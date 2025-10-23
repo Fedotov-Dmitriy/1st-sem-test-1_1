5f48773c81506cef7192e3ccf122c90060de165b
Georgiy Mikhailenko <kislball@yahoo.com>
2025-10-23 14:55:09 +0300

    Add more tests for Luhn's algorithm

--- test/test_luhn.py
+++ test/test_luhn.py
@@ -9,8 +9,8 @@ def test_only_whitespace():
 
 
 def test_spacing():
-    assert luhn_check("8571261212345467")
-    assert luhn_check("8571 2612 1234 5467")
+    assert not luhn_check("8571261212345467")
+    assert not luhn_check("8571 2612 1234 5467")
     with pytest.raises(ValueError):
         assert luhn_check("857  12612 1  2345467")
         assert luhn_check("8571 2612 123454 67")
@@ -21,8 +21,11 @@ def test_too_many_digits():
         assert luhn_check("11111111111111111111111111111")
 
 def test_good():
-    assert luhn_check("8571 2612 1234 5467")
-
+    assert luhn_check("4263 9826 4026 9299")
+    assert luhn_check("4242 4242 4242 4242")
+    assert luhn_check("4539 3195 0343 6467")
 
 def test_bad():
-    assert not luhn_check("4561 2612 1234 5463")
+    assert not luhn_check("4223 9826 4026 9299")
+    assert not luhn_check("4539 3195 0343 6476")
+    assert not luhn_check("8273 1232 7352 0569")
