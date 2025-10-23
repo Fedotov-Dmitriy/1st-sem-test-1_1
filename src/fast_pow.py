5fc7ed5a0062e9e0578585ffe8f179ab27cf9764
Yana Kalsina <yaprogrammer18@gmail.com>
2025-10-23 14:55:26 +0300

    Добавила исключений.

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,7 +1,13 @@
+class MyException(Exception):
+    pass
+
 def fastPow(number, power):
+    if power < 0:
+        raise MyException("Отрицательная степень")
+    if power > 1000000000:
+        raise MyException("Слишком большая степень")
     result = 1
     while power != 0:
         result *= number
         power = power - 1
     return result
-
