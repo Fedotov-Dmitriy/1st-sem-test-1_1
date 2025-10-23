351ba7472a2b307d9bcff4ea5ba46728d09bffe3
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-10-23 14:38:21 +0300

    Fixed tests bugs
    Changed testscase because it was invalid.

--- test/test_luhn.py
+++ test/test_luhn.py
@@ -2,8 +2,11 @@ from src.luhn import luhn_check
 
 
 def test_good():
-    assert luhn_check("8571 2612 1234 5467")
+    assert luhn_check("8571 2612 1234 5466")
+    assert luhn_check("4  5  6  1     2  6  1  2     1  2  3  4     5  5  6  6")
 
 
 def test_bad():
     assert not luhn_check("4561 2612 1234 5463")
+    assert not luhn_check("4  5  6  1     2  6  1  2     1  2  3  4     5  4  6  4")
+    assert not luhn_check("4  5  6  1     2  6  1  2     1  2  3  4     5  4  6  7")
