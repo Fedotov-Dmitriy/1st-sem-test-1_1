a410ff543d93e73f155594b019cb6960b8f722eb
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-10-23 15:01:29 +0300

    Fixed bug in luhn check
    Индекс начинается с нуля, поэтому его остаток от деления на два должен быть НЕ равен остатку от деления длины номера минус один на два.

--- src/luhn.py
+++ src/luhn.py
@@ -4,7 +4,7 @@ def luhn_check(card_number):
     parity = (len(digits)) % 2
     total = 0
     for i in range(len(digits)):
-        if i % 2 == parity:
+        if i % 2 != parity:
             doubled = digits[i] * 2
             if doubled > 9:
                 doubled -= 9
