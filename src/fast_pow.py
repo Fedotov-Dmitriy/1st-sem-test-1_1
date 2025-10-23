086eec46edec64ba588dfd4af3197adddaa804cd
Max Ipatov <imaxde@mail.ru>
2025-10-23 15:02:21 +0300

    Exception for negative power

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,4 +1,10 @@
+class NegativePowerException(Exception):
+    pass
+
+
 def fast_pow(number, power):
+    if power < 1:
+        raise NegativePowerException
     result = number
     while power != 1:
         result *= result
