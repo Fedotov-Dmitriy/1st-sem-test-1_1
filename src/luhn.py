daefb7e08114469da00e736246d04f3425ae9578
Georgiy Mikhailenko <kislball@yahoo.com>
2025-10-23 14:00:39 +0300

    Add checks to Luhn's algorithm

--- src/luhn.py
+++ src/luhn.py
@@ -1,5 +1,14 @@
 def luhn_check(card_number):
+    if not all(x.isdigit() or x.isspace() for x in str(card_number)):
+        raise ValueError("card_number should only consist of numbers and whitespace characters")
+    spaces = sum(1 for x in str(card_number) if x.isspace())
+    if spaces != 0 and spaces != 3:
+        raise ValueError("card_number should only have zero or three whitespace characters")
+
     digits = [int(d) for d in str(card_number) if d.isdigit()]
+    if len(digits) != 16:
+        raise ValueError("card_number should have exactly 16 digits")
+
     control = digits.pop()
     parity = (len(digits))%2
     total = 0
