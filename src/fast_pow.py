1f79e8d78a26a0c848af57c44cd5794c9f8d2353
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-10-23 15:10:01 +0300

    Fixed previous stupid commit for not resolving conflicts.

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,11 +1,5 @@
 def fast_pow(number, power):
-<<<<<<< HEAD
     """
     Mega fast pow algorithm.
     """
-=======
-    '''
-    Mega fast pow algorithm.
-    '''
->>>>>>> 01c7ae66cbdb9a24aa7b3c1c582061869455c3ea
     return number**power
