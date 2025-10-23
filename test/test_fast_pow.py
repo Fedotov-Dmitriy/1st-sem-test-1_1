e7a4dfc2ec501e32dea6668c583d46ac9bfb5722
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-10-23 14:05:59 +0300

    Added epsilon for floating numbers

--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -1,6 +1,5 @@
 from src.fast_pow import fast_pow
 
-
 def test_two_power_two():
     assert fast_pow(2, 2) == 4
 
@@ -10,12 +9,14 @@ def test_negative():
     assert fast_pow(-2, 3) == -8
 
 def test_negative_power():
-    assert fast_pow(5,-1) == 0.2
-    assert fast_pow(10, -2) == 0.01
-    assert fast_pow(77, -5) == 77**(-5)
+    epsilon = 1e-8
 
-    assert fast_pow(-0.2, -2) == 25
-    assert fast_pow(-0.2, -1) == 5
+    assert fast_pow(5,-1) - 0.2 <= epsilon
+    assert fast_pow(10, -2) - 0.01 <= epsilon
+    assert fast_pow(77, -5) - 77**(-5) <= epsilon
+
+    assert fast_pow(-0.2, -2) - 25 <= epsilon
+    assert fast_pow(-0.2, -1) - 5 <= epsilon
 
 def edge_cases():
     assert fast_pow(1100101, 0) == 1
