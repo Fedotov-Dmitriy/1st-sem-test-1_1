338eeff6656aabae4a810098fee9e3dbaf6d0bc6
Maria Byvalkina <masabyvalkina67715@gmail.com>
2025-10-23 15:14:39 +0300

    Добавили исправленный файл fast_pow, где исправили ошибку с отступами

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,12 +1,16 @@
 def fast_pow(number, power):
 
-result = number
+    result = number
+
     if power < 0:
         raise InvalidPowerError("Отрицательная степень не поддерживается")
+
     if not isinstance(power, int):
         raise TypeError("Степень всегда является целым числом")
+
     if not isinstance(number, int):
         raise TypeError("Число, возводимое в степень должно быть целым")
+
     while power != 1:
         result *= result
         power = power // 2
