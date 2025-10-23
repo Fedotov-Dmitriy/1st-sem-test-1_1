be02b56cbc9b9b525a40a396804d661a160a98f9
Andrew-Kochanov <andkochanov2019@gmail.com>
2025-10-23 15:15:01 +0300

    Быстрое возведение в степень
    Сделал так, чтобы result умножался на number

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,9 @@
 def fastPow(number, power):
     result = number
     while power != 1:
-        result *= result
-        power = power // 2
+        result *= number
+        power = power - 1
+        print(result, power)
     return result
+
+
