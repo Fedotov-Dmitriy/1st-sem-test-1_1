2129eef504801f1a7936b4a49cdd90fe43d43887
Georgiy Mikhailenko <kislball@yahoo.com>
2025-10-23 14:30:27 +0300

    Add more tests for fast_pow

--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -4,6 +4,14 @@ from src.fast_pow import fast_pow
 def test_two_power_two():
     assert fast_pow(2, 2) == 4
 
+def test_zero():
+    assert all(fast_pow(1, x) == 1 for x in range(1, 100))
+    assert all(fast_pow(x, 0) == 1 for x in range(1, 100))
+
+def test_fuzzy():
+    for x in range(1, 10):
+        for y in range(1, 10):
+            assert fast_pow(x, y) == x**y
 
 def test_negative():
     assert fast_pow(-1, 4) == 1
