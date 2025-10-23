a503ae468ae30d6c0c6daa36eefaa0d533fa2591
Nikolay Baboshin <231540704+shknoko@users.noreply.github.com>
2025-10-23 15:11:27 +0300

    Переписан алгоритм быстрого возведения в степень

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,12 @@
 def fast_pow(number, power):
-    result = number
-    while power != 1:
-        result *= result
+    if power < 1:
+        raise ValueError("Степень меньше 1")
+    result = 1
+    base = number
+    while power > 0:
+        if power % 2 == 1:
+            result *= base
+            power -= 1
+        base *= base
         power = power // 2
     return result
