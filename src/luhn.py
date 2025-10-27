db98e48b95491e39e33ab589e77ed34a0626ba37
Fedotov-Dmitriy <qwertyyy20262@mail.ru>
2025-10-27 16:32:29 +0300

    Добавил реализацию утилиты

--- src/luhn.py
+++ src/luhn.py
@@ -1,3 +1,5 @@
+
+
 def luhn_check(card_number):
     digits = [int(d) for d in str(card_number) if d.isdigit()]
     control = digits.pop()
@@ -12,3 +14,19 @@ def luhn_check(card_number):
         else:
             total += digits[i]
     return (total + control) % 10 == 0
+
+def utility():
+    while True:
+        number = input("Введите номер карты: ").strip()
+        if number == "-1":
+            print("Выход из программы.")
+            break
+        try:
+            if luhn_check(number):
+                print("correct")
+            else:
+                print("incorrect")
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")
+
+utility()
